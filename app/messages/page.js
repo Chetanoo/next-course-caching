1b58264d525368d18204a6d1f2eb77a54c8a0db6
KirillGorinDataArt <chetanvse@gmail.com>
2025-07-20 19:41:10 +0400

    caching data from custom data source with react cache

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,19 +1,22 @@
 import Messages from "@/components/messages";
 import { unstable_noStore } from "next/cache";
+import { getMessages } from "@/lib/messages";
 
 // export const revalidate = 5; // reserved name, doing the same as revalidate in fetch options
 // export const dynamic = "force-dynamic";
 
-export default async function MessagesPage() {
-  unstable_noStore();
-  const response = await fetch("http://localhost:8080/messages", {
-    // cache: "no-store",
-    // next: {
-    //   revalidate: 5, // amount of seconds to use cache before revalidation
-    //   tags: ['msg'],
-    // },
-  });
-  const messages = await response.json();
+export default function MessagesPage() {
+  // unstable_noStore();
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   // cache: "no-store",
+  //   // next: {
+  //   //   revalidate: 5, // amount of seconds to use cache before revalidation
+  //   //   tags: ['msg'],
+  //   // },
+  // });
+  // const messages = await response.json();\
+
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
